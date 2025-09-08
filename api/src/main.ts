b7027970838cd1bf0e963a2fa830f970c7acad9f
Franco de Iriondo <luafranco09@hotmail.com>
2025-09-08 01:11:20 -0300

    Test

--- api/src/main.ts
+++ api/src/main.ts
@@ -15,11 +15,19 @@ async function bootstrap() {
   }),
 );
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://dsn-test.vercel.app'],
-    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  origin: (origin, callback) => {
+    const allowedOrigins = ['http://localhost:4200', 'https://dsn-test.vercel.app'];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+});
+
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
