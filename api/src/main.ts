bfca0cb8e6fdc1743cec3cd97ca0b855ecede1e2
Franco de Iriondo <luafranco09@hotmail.com>
2025-09-08 00:53:19 -0300

    Test

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,10 +7,27 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true, // permite cualquier origen
+    origin: ['http://localhost:4200', 'https://dsn-test.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: '*',
     credentials: true,
+  });
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
   });
 
   app.useGlobalPipes(
